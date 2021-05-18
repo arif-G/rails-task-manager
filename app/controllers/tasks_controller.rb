ec7ed5d775a8e1fd25eacad780e7346d7142f01c
Arif Gömleksiz <arifgoemleksiz101@gmail.com>
2021-05-18 14:24:16 +0200

    before actionr efactor

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,10 @@
 class TasksController < ApplicationController
+  before_action :find_task, only: %i[show edit update destroy]
   def index
     @tasks = Task.all
   end
 
-  def show
-    @task = Task.find(params[:id])
-  end
+  def show; end
 
   def new
     @task = Task.new
@@ -17,26 +16,25 @@ class TasksController < ApplicationController
     redirect_to tasks_path
   end
 
-  def edit
-    @task = Task.find(params[:id])
-  end
+  def edit; end
 
   def update
-    @task = Task.find(params[:id])
     @task.update(task_params)
     redirect_to task_path(@task)
   end
 
   def destroy
-    @task = Task.find(params[:id])
     @task.delete
     redirect_to tasks_path
   end
+
   private
+
+  def find_task
+    @task = Task.find(params[:id])
+  end
 
   def task_params
     params.require(:task).permit(:title, :details, :completed)
   end
-
-
 end
